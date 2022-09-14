e4da7237b05204e790f987a60916d021951d6202
Rodrigo <rodrigorael53@gmail.com>
2022-09-13 21:58:30 -0300

    finishing the logoImg and the h1

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,17 @@
 import './styles/main.css';
 
+import logoImg from './assets/logo-nlw-esports.svg';
+
 function App() {
   return (
-    <div className="w-8 h-8 bg-violet-500">
+    <div 
+      className="max-w-[1344px] mx-auto flex flex-col items-center my-20"
+    >
+      <img src={logoImg} alt="" />
 
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+      </h1>
     </div>
   );
 }
