56ab9cd106f003c8d985b11602db004af10b53ab
Rodrigo <rodrigorael53@gmail.com>
2022-09-15 11:02:45 -0300

    creating the GameBanner component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
 import { MagnifyingGlassPlus } from 'phosphor-react';
 
+import { GameBanner } from './components/GameBanner';
+
 import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg';
@@ -16,131 +18,7 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-1.png" alt="" />
-
-          <div 
-            className="
-              w-full 
-              pt-16 
-              pb-4 
-              px-4 
-              bg-game-gradient 
-              absolute 
-              bottom-0 
-              left-0 
-              right-0
-            "
-          >
-            <strong className="font-bold text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-2.png" alt="" />
-
-          <div 
-            className="
-              w-full 
-              pt-16 
-              pb-4 
-              px-4 
-              bg-game-gradient 
-              absolute 
-              bottom-0 
-              left-0 
-              right-0
-            "
-          >
-            <strong className="font-bold text-white block">Dota 2</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-3.png" alt="" />
-
-          <div 
-            className="
-              w-full 
-              pt-16 
-              pb-4 
-              px-4 
-              bg-game-gradient 
-              absolute 
-              bottom-0 
-              left-0 
-              right-0
-            "
-          >
-            <strong className="font-bold text-white block">Counter Strike</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-4.png" alt="" />
-
-          <div 
-            className="
-              w-full 
-              pt-16 
-              pb-4 
-              px-4 
-              bg-game-gradient 
-              absolute 
-              bottom-0 
-              left-0 
-              right-0
-            "
-          >
-            <strong className="font-bold text-white block">Apex Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-5.png" alt="" />
-
-          <div 
-            className="
-              w-full 
-              pt-16 
-              pb-4 
-              px-4 
-              bg-game-gradient 
-              absolute 
-              bottom-0 
-              left-0 
-              right-0
-            "
-          >
-            <strong className="font-bold text-white block">Fortnite</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-
-        <a href="" className="relative rounded-lg overflow-hidden">
-          <img src="/game-6.png" alt="" />
-
-          <div 
-            className="
-              w-full 
-              pt-16 
-              pb-4 
-              px-4 
-              bg-game-gradient 
-              absolute 
-              bottom-0 
-              left-0 
-              right-0
-            "
-          >
-            <strong className="font-bold text-white block">World of Warcraft</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
+        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
       </div>
 
       <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
