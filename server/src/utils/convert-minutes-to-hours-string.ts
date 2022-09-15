f11f1baa3febbab4e83cf622c186267fa6affc52
Rodrigo <rodrigorael53@gmail.com>
2022-09-15 06:46:58 -0300

    creating the app routes

--- server/src/utils/convert-minutes-to-hours-string.ts
+++ server/src/utils/convert-minutes-to-hours-string.ts
@@ -3,5 +3,7 @@ export function convertMinutesToHoursString(minutesAmount: number) {
   // pegando o resto da divisão
   const minutes = minutesAmount % 60;
 
-  return `${hours}:${minutes}`;
+  // o padStart vai adicionar 0 na frente no número de horas caso o número de
+  // horas não tenha 2 caracteres
+  return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
 };
