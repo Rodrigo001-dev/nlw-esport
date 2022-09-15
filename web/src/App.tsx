d2020a53ebb28c968d63e7288f1f859781576f24
Rodrigo <rodrigorael53@gmail.com>
2022-09-15 12:23:55 -0300

    creating the CreateAdBanner component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { MagnifyingGlassPlus } from 'phosphor-react';
 
 import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 import './styles/main.css';
 
@@ -19,29 +20,10 @@ function App() {
 
       <div className="grid grid-cols-6 gap-6 mt-16">
         <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
+        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={2} />
       </div>
 
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
-        <div 
-          className="bg-[#2A2634] px-8 py-6 flex justify-between items-center"
-        >
-          <div>
-            <strong className="text-2xl text-white font-black block">
-              Não encontrou seu duo?
-            </strong>
-            <span className="text-zinc-400 block">
-              Publique um anúncio par aencontrar novos players!
-            </span>
-          </div>
-
-          <button 
-            className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3"
-          >
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner />
     </div>
   );
 }
