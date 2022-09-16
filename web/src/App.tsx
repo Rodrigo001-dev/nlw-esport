1f815559bd7243c50c8cf5b6780e1c12eeddfe3c
Rodrigo <rodrigorael53@gmail.com>
2022-09-16 00:52:47 -0300

    getting the games from the backend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { MagnifyingGlassPlus } from 'phosphor-react';
+import { useEffect, useState } from 'react';
 
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
@@ -7,7 +7,26 @@ import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg';
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number
+  };
+};
+
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, []);
+
   return (
     <div 
       className="max-w-[1344px] mx-auto flex flex-col items-center my-20"
@@ -19,8 +38,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
-        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={2} />
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl} 
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
 
       <CreateAdBanner />
